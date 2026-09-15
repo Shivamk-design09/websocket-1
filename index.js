12e681918d2d5a52c8d033627bf4d12c21ac9728
shivam <shivamk44409@gmail.com>
2026-09-15 09:01:24 +0530

    done

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const http = require("http")
+const path = require("path")
+const { Server } = require("socket.io")
 const app = express()
 const server = http.createServer(app)
+// is http server is saath socket io bhi attach kardo
+const io = new Server(server)
+app.use(express.static(path.resolve('./public')))
 
 
-app.use(express.static("/public"))
-
-
-app.get('/',(req,res)=>{
-    res.sendFile('/public/index.html')
+// socket io connection
+// socket is like an user
+io.on('connection', (socket) => {
+    console.log('new user is connected', socket.id)
+    socket.on('user-message', (message) => {
+        io.emit('message', message)
+    })
 })
 
-server.listen(9000,()=>console.log(`server is listening on PORT:9000`))
+app.get('/', (req, res) => {
+    return res.sendFile('./public/index.html')
+})
+
+
+
+server.listen(9000, () => console.log(`server is listening on PORT:9000`))
